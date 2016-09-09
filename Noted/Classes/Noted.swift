f44d8bcf7c678de75c0a4e2ac8c08d4fcc39ab16
Todor Dimitrov Brachkov <tobr@nodesagency.com>
2016-09-09 15:30:31 +0100

    -Make Noted singleton safe -Thread safety -Unit tests (just for multiple queue usage)

--- Noted/Classes/Noted.swift
+++ Noted/Classes/Noted.swift
@@ -15,19 +15,32 @@ public class Noted {
     
     public static let defaultInstance = Noted()
     
+    private let notedQueue = dispatch_queue_create("com.noted.queue", nil)
+    
+    private init() {}
+    
     var receivers = NSHashTable(options: NSPointerFunctionsOptions.WeakMemory)
     
     public func addObserver(observer: AnyObject) {
-        receivers.addObject(observer)
+        dispatch_sync(notedQueue) { [weak self] in
+            self?.receivers.addObject(observer)
+        }
     }
     
     public func removeObserver(observer: AnyObject) {
-        receivers.removeObject(observer)
+        dispatch_sync(notedQueue) { [weak self] in
+            self?.receivers.removeObject(observer)
+        }
     }
     
     public func postNotification(notification: Notification) {
-        for receiver in receivers.allObjects {
-            notification.trigger(receiver)
+        dispatch_sync(notedQueue) { [weak self] in
+            for receiver in self?.receivers.allObjects ?? [] {
+                dispatch_async(dispatch_get_main_queue()) { [weak receiver] in
+                    guard let receiver = receiver else { return }
+                    notification.trigger(receiver)
+                }
+            }
         }
     }
 }
