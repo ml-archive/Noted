020cd103007be1ad58ff7812729179dbc940368b
Jakob Mygind <Jmje@nodes.dk>
2016-09-13 13:11:08 +0200

    now using dispatch_barrier_async

--- Noted/Classes/Noted.swift
+++ Noted/Classes/Noted.swift
@@ -8,33 +8,33 @@
 import Foundation
 
 public protocol Notification {
-    func trigger(reciever: AnyObject)
+    func trigger(receiver: AnyObject)
 }
 
 public class Noted {
     
     public static let defaultInstance = Noted()
     
-    private let notedQueue = dispatch_queue_create("com.noted.queue", nil)
+    private let notedQueue = dispatch_queue_create("com.noted.queue", DISPATCH_QUEUE_CONCURRENT)
     
     private init() {}
     
     var receivers = NSHashTable(options: NSPointerFunctionsOptions.WeakMemory)
     
     public func addObserver(observer: AnyObject) {
-        dispatch_sync(notedQueue) { [weak self] in
+        dispatch_barrier_async(notedQueue) { [weak self] in
             self?.receivers.addObject(observer)
         }
     }
     
     public func removeObserver(observer: AnyObject) {
-        dispatch_sync(notedQueue) { [weak self] in
+        dispatch_barrier_async(notedQueue) { [weak self] in
             self?.receivers.removeObject(observer)
         }
     }
     
     public func postNotification(notification: Notification) {
-        dispatch_sync(notedQueue) { [weak self] in
+        dispatch_async(notedQueue) { [weak self] in
             for receiver in self?.receivers.allObjects ?? [] {
                 dispatch_async(dispatch_get_main_queue()) { [weak receiver] in
                     guard let receiver = receiver else { return }
